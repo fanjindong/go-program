d6ab2a93e8d6d9f66075c63005fb77691b922ccc
fanjindong <765912710@qq.com>
2019-09-05 16:12:36 +0800

    feat: add RWMutex

--- lock/lock_test.go
+++ lock/lock_test.go
@@ -32,3 +32,30 @@ func TestMutex(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	assert.Assert(t, count == N, count)
 }
+
+func TestRWMutex(t *testing.T) {
+	count = 0
+	var assertCount int
+	N := 1000
+	mutex := &sync.RWMutex{}
+	for i := 0; i < N; i++ {
+		go func() {
+			mutex.RLock()
+			assert.Assert(t, count == assertCount, count)
+			t.Log(count)
+			mutex.RUnlock()
+		}()
+	}
+	N = N / 100
+	for i := 0; i < N; i++ {
+		go func() {
+			mutex.Lock()
+			count++
+			assertCount++
+			t.Log(count, assertCount)
+			mutex.Unlock()
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	assert.Assert(t, count == N, count)
+}
