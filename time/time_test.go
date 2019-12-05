adb06df4f4d5b1c7a96d1874025a7369c3e151d5
fanjindong <765912710@qq.com>
2019-12-05 18:31:05 +0800

    fix: add time.Ticker

--- time/time_test.go
+++ time/time_test.go
@@ -1,6 +1,7 @@
 package time_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -8,4 +9,10 @@ import (
 func TestTime(t *testing.T) {
 	now := time.Now()
 	t.Log(time.Date(now.Year()-18, now.Month(), now.Day(), 0, 0, 0, 0, time.Local).Format("2006-01-02"))
+}
+
+func TestTimeTicker(t *testing.T) {
+	for range time.NewTicker(3 * time.Second).C {
+		fmt.Println(time.Now())
+	}
 }
